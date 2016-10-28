39b0763c75927830ca69ad8450796916c688281f
ruben elizondo <ruben.elizondo@utexas.edu>
2016-10-28 00:27:34 +0000

    commented out sqlaclchemy-searchable, not being used and causing error in app.py, changed __name__ in loader to make debugging easier, based on api reading, http://flask.pocoo.org/docs/0.11/api/#template-rendering

--- app/app.py
+++ app/app.py
@@ -1,16 +1,17 @@
 from models import Artist, Album, Track
-import requests
-from sqlalchemy_searchable import parse_search_query, search
+#from sqlalchemy_searchable import parse_search_query, search
 from loader import app, db
+from flask import send_file
 
 #-----------
 # view pages
 #-----------
 
+
+
 @app.route('/sanity', methods=['GET'])
 def sanity():
 	return "This is a sanity check!"
-
 
 @app.route('/index.html', methods=['GET'])
 def index_exp():
@@ -20,6 +21,7 @@ def index_exp():
 @app.route('/', methods=['GET'])
 def index():
 	return send_file('index.html')
+
 
 if __name__ == "__main__":
 	app.run()
