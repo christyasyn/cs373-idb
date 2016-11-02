b0dc8cc8546cd0e156306cbd79c5bdf208190a4b
ruben elizondo <ruben.elizondo@utexas.edu>
2016-11-02 16:49:23 +0000

    working on dynamic table

--- app/app.py
+++ app/app.py
@@ -3,23 +3,32 @@ from models import Artist, Album, Track
 from loader import app, db
 from flask import send_file, jsonify, render_template
 import json
+import pickle
+import sys
 #-----------
 # view pages
 #-----------
 
-
 artist_data = {}
 
 def prelist_test():
-        global artist_data
-        artists = Artist.query.all()
-        artist_data['aaData'] = [artist.to_list() for artist in artists]
-        artist_data['columns'] = [
-              { "title": "Artist" },
-              { "title": "Genre" } ,
-              { "title": "Followers" },
-              { "title": "Popularity" }
-             ]
+	artists = Artist.query.all()
+	global artist_data
+	artist_data = {}
+	artist_data['aaData'] = [artist.to_list() for artist in artists]
+	artist_data['columns'] = [
+		{ "title": "Artist" },
+		{ "title": "Genre" } ,
+		{ "title": "Followers" },
+		{ "title": "Popularity" }
+		]
+	with open('./db/all_artist_query.pickle', 'wb') as out:
+		pickle.dump(artist_data, out)
+	with open('./db.all_artist_query.txt', 'w') as out:
+		out.write(json.dumps(artist_data))
+	return artist_data
+
+
 
 @app.route('/sanity', methods=['GET'])
 def sanity():
@@ -31,9 +40,17 @@ def index():
 
 @app.route("/artists", methods=['GET'])
 def get_artists():
-	prelist_test()
+	global artist_data
+	if len(artist_data) == 0:
+		try:
+			with open('./db/all_artist_query.pickle', 'rb') as read:
+				artist_data = dict(pickle.load(read))
+		except:
+			artist_data = prelist_test()
+
+	return render_template('test.html', test_output=str(len(artist_data)))
 	# return json.dumps({'artists': [artist.to_json() for artist in artists]})
-	return render_template('artists.html', artists=jsonify(artist_data))
+	# return render_template('artists.html', artists=json.dumps(artist_data))
 
 
 @app.route('/run_unittests')
