6ab74dd08cc4ba9f80f75686470c9861f141670a
ruben elizondo <ruben.elizondo@utexas.edu>
2016-11-14 22:09:27 +0000

    boilerplate.py tabsgit add boilerplate.py

--- app/boilerplate.py
+++ app/boilerplate.py
@@ -1,47 +1,51 @@
 #import requests
 import random
-import urllib3
+#import urllib.request
+import urllib3.PoolManger
 import json
 
 EASY_STEPS = 4
 HARD_STEPS = 8
 
 cuisines = {1:"African",
-			2:"American",
+                        2:"American",
 			3:"British",
-			4:"Cajun",
-			5:"Carribean",
-			6:"Chinese",
-			7:"Eastern European",
-			8:"French",
-			9:"German",
-			10:"Greek",
-			11:"Indian",
-			12:"Irish",
-			13:"Italian",
-			14:"Japanese",
-			15:"Jewish",
-			16:"Korean",
-			17:"Latin American",
-			18:"Mexican",
-			19:"Middle Eastern",
-			20:"Nordic",
-			21:"Southern",
-			22:"Spanish",
-			23:"Thai",
-			24:"Vietnamese"
-			}
+                        4:"Cajun",
+                        5:"Carribean",
+	                6:"Chinese",
+                        7:"Eastern European",
+                        8:"French",
+                        9:"German",
+                       10:"Greek",
+                       11:"Indian",
+                       12:"Irish",
+                       13:"Italian",
+                       14:"Japanese",
+                       15:"Jewish",
+                       16:"Korean",
+                       17:"Latin American",
+                       18:"Mexican",
+                       19:"Middle Eastern",
+                       20:"Nordic",
+                       21:"Southern",
+                       22:"Spanish",
+                       23:"Thai",
+                       24:"Vietnamese"
+}
 
 def get_cuisine_recipes(cuisine,difficulty):
 	cuisines_url = "http://boilerpl8.me/api/cuisines"
 	#cr = requests.get(cuisines_url)
-	http = urllib3.PoolManager()
-	cr = http.request('GET', cuisines_url)
-	cx = json.loads(cr.data.decode('utf-8'))
+        http = urllib3.PoolManager()
+        cr = http.request('GET',cuisines_url)
+        cx = json.loads(cr.data.decode('utf-8')
+	#cr = urllib.request.urlopen(cuisines_url)
+	#cr = http.request('GET', cuisines_url)
+	#cx = json.load(cr.data)
 	cl = cx['cuisines']
 	recipes_url = "http://boilerpl8.me/api/recipes/"
-	rr = http.request('GET', recipes_url)
-	rx = json.loads(rr.data.decode('utf-8'))
+	rr = urllib.request.urlopen(recipes_url)
+	rx = json.load(rr.data)
 	rl = rx['recipes']
 	if cuisine != 0:
 		id = 1
